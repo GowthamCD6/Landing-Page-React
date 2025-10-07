e3f5d8b6ece834831b7d9b908e9b1e9ef266fb58
GowthamCD6 <gowthamnaveen124@gmail.com>
2025-10-07 09:39:26 +0530

    depeloyement 1

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Title from './component/Title/Title'
 import About from './component/About/About'
 import Travel from './component/Travel/Travel'
 import Slider from './component/Slider/Slider'
-import Testimonial from './component/testimonial/Testimonial'
+import Testimonial from './component/Testimonial/Testimonial'
 
 
 const App = () => {
